c0a6deabbe483258caba85ef2157fccafeff7e8d
ZengTao <taozeng@mindhand.io>
2020-05-04 12:59:32 +0800

    container base remove healthy check

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,17 @@
 
 ActiveRecord::Schema.define(version: 2020_04_13_141649) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.string "avatar"
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "period_id"
-    t.integer "subscription_id"
+    t.bigint "period_id"
+    t.bigint "subscription_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["period_id"], name: "index_orders_on_period_id"
@@ -32,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_04_13_141649) do
   end
 
   create_table "statistics_item_clicks", force: :cascade do |t|
-    t.integer "item_id"
+    t.bigint "item_id"
     t.integer "clicked", default: 0
     t.date "date"
     t.index ["item_id", "date"], name: "idx_item_clicks_uniq", unique: true
@@ -40,7 +43,7 @@ ActiveRecord::Schema.define(version: 2020_04_13_141649) do
   end
 
   create_table "subscriptions", force: :cascade do |t|
-    t.boolean "terminated", default: false
+    t.boolean "terminated", default: false, comment: "当设置为 True 的时候代表订阅已经被终止"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["terminated"], name: "idx_terminated_on_subscription"
